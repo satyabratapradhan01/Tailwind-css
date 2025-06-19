17249e86f48a023c43d4ded35fb7c8d10b8a361e
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-19 23:56:50 +0530

    header

---
+++ tailwindcss/src/Component/Header.jsx
@@ -0,0 +1,13 @@
+
+export const Header = () => {
+    return(
+        <>
+        <ul className="bg-black text-white flex m-0 [&>*]:flex">
+            <li><a href="#" className="p-3 hover:bg-zinc-900">Home</a></li>
+            <li><a href="#" className="p-3 hover:bg-zinc-900">News</a></li>
+            <li><a href="#" className="p-3 hover:bg-zinc-900">Contact</a></li>
+            <li><a href="#" className="p-3 hover:bg-zinc-900">About</a></li>
+        </ul>
+        </>
+    )
+}
